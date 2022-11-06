54a1578bc1ab4dc7bfaf724eb43971224abca085
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-11-06 14:10:35 -0800

    set up bootstrap navbar w/ vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <navigation-bar></navigation-bar>
+  <!-- <div class="container">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div> -->
+  <div class="pt-5">
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import NavigationBar from './components/NavigationBar.vue'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    NavigationBar
   }
 }
+</script>
+
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+
 </style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-light">
+        <div class="container">
+            <router-link 
+                to="/"><img alt="Bitbuddy logo" 
+                src="../assets/logo-orange.png"
+                class="navbar-brand"
+            ></router-link>
+            <button 
+                class="navbar-toggler" 
+                type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarNavAltMarkup" 
+                aria-controls="navbarNavAltMarkup" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <router-link class="nav-link" to="/">Home</router-link>
+                    <router-link class="nav-link" to="/about">About</router-link></div>
+                </div>
+            </div>
+    </nav>
+</template>
+
+<style lang="scss">
+nav {
+  padding: 30px;
+    
+  a {
+    //color: #2c3e50;
+    // &.router-link-exact-active {
+    //   color: #42b983;
+    // }
+  }
+  img {
+    max-height: 70px;
+  }
+}
+</style>
