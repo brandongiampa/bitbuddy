9cec71c9ba1b8852836be0a9b8b366fb7d471644
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-11-07 12:39:08 -0800

    create text input of type 'number' to input currency amount

---
+++ src/components/currencyconversionformcomponents/CurrencyAmountTextInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <input 
+            type="number" 
+            class="w-100" 
+            v-model="currencyAmount"
+            @change="changeAmount"
+        >
+    </div>
+</template>
+
+<script lang="js">
+export default {
+    data() {
+        return {
+            currencyAmount: this.defaultAmount
+        }
+    },
+    methods: {
+        changeAmount() {
+            this.$emit('transmitAmountChange', this.currencyAmount)
+        }
+    },
+    props: {
+        defaultAmount: {
+            type: Number,
+            required: false,
+            default: 100
+        }
+    },
+    emits: [
+        'transmitAmountChange'
+    ]
+}
+</script>
