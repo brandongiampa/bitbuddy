55c8dfc9a62bba71b78455b2d5a0331ca71d6c14
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-11-20 20:54:26 -0800

    make commented-out versions of routes for build

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,11 +4,13 @@ import HomeView from '../views/HomeView.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
+    //path: '/bitbuddy',
     name: 'home',
     component: HomeView
   },
   {
     path: '/about',
+    //path: '/bitbuddy/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
